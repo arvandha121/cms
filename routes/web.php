8577adea0c17ce264172b9ce1c973cdd6fe79ca9
arvandha121 <nauvan121@gmail.com>
2022-02-23 14:34:23 +0700

    Routing Web Framework Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return "Hi! Selamat Datang di Website Laravel";
+});
+
+Route::get('/about', function () {
+    return "
+    NIM     : 2041720134
+    Nama    : Arief Nauvan Ramadha
+    Kelas   : TI - 2G";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Ini adalah halaman Artikel dengan id " . $id;
 });
